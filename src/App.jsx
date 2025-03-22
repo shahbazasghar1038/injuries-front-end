0ab3e0fb178463276ed99ac4c1bc00cbfce5a37f
Mtalhaikram <109526501+Mtalhaikram@users.noreply.github.com>
2025-03-23 00:01:29 +0500

    signup page start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import SignIn from './pages/AuthPages/SignIn'
 import ForgotPassword from './pages/AuthPages/ForgotPassword'
+import SignUp from './pages/AuthPages/SignUp'
+
 function App() {
 
   return (
     <>
       {/* <SignIn /> */}
-      <ForgotPassword />
+      {/* <ForgotPassword /> */}
+      <SignUp />
     </>
   )
 }
