4a78b85230cce62924f6d805d65c84bbdd2339ff
mahtab541994 <71431688+mahtab541994@users.noreply.github.com>
2025-03-29 17:15:03 -0700

    ongoing case breadcrumb

--- src/pages/OngoingCases/Index.jsx
+++ src/pages/OngoingCases/Index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import AuthenticatedLayout from '../../layout/AuthenticatedLayout'
+import Breadcrumb from './partials/Breadcrumb';
 
 const OngoingCases = () => {
+  const breadcrumbLinks = [
+    { label: "Home", href: "/" },
+    { label: "Ongoing Cases", href: "/cases" },
+    { label: "Case Details" },
+  ];
   return (
     <AuthenticatedLayout>
-        <h2>Ongoing Cases</h2>
+      <div className='flex justify-between'>
+
+        <p className='page-heading'>Ongoing Cases</p>
+        <Breadcrumb  links={breadcrumbLinks} />
+      </div>
     </AuthenticatedLayout>
   )
 }
