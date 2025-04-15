c8d058f6f29aa6bb69d2befc1ec7d846eeb2e1aa
Mtalhaikram <109526501+Mtalhaikram@users.noreply.github.com>
2025-04-16 01:19:13 +0500

    Update AuthenticatedLayout.jsx

--- src/layout/AuthenticatedLayout.jsx
+++ src/layout/AuthenticatedLayout.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './Sidebar'
 import Header from './Header'
 
 const AuthenticatedLayout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <div className='layout-flex'>
-        <Sidebar />
+        <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
         <div className="layout-padding w-full">
-            <Header />
+            <Header toggleSidebar={toggleSidebar} />
 
             <div className="content-wrapper content-padding">
                 {children}
