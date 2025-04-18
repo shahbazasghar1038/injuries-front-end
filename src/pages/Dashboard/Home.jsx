60d77bcc2d10cc86eff2001c4753a48730d7c09f
Mtalhaikram <109526501+Mtalhaikram@users.noreply.github.com>
2025-04-18 22:18:45 +0500

    Update Home.jsx

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -182,6 +182,25 @@ const Home = () => {
     setOpenQuestions({});
   };
 
+  // Add state for accept confirmation modal
+  const [acceptModalVisible, setAcceptModalVisible] = useState(false);
+  
+  // Function to handle accept button click
+  const handleAcceptClient = () => {
+    if (!selectedContact) return;
+    // Show acceptance confirmation modal
+    setAcceptModalVisible(true);
+  };
+  
+  // Function to confirm client acceptance
+  const confirmAcceptClient = () => {
+    // Implement any logic needed for accepting the client
+    console.log(`Client ${selectedContact.name} accepted`);
+    
+    // Close the modal
+    setAcceptModalVisible(false);
+  };
+
   return (
     <AuthenticatedLayout>
       <style>{dropdownAnimation}</style>
@@ -350,7 +369,10 @@ const Home = () => {
                       </div>
                     </div>
 
-                    <button className="btn btn-primary btn-sm">
+                    <button 
+                      className="btn btn-primary btn-sm accept-btn"
+                      onClick={handleAcceptClient}
+                    >
                       Accept
                     </button>
                   </div>
@@ -497,6 +519,22 @@ const Home = () => {
         content={`Lorem ipsum dolor sit amet consectetur. Feugiat ipsum libero tempor felis
                   risus nisi non. Quisque eu ut tempor curabitur.`}
         showButtons={true}
+      />
+      
+      {/* Accept confirmation modal */}
+      <ActionModal 
+        open={acceptModalVisible}
+        onCancel={() => setAcceptModalVisible(false)}
+        onConfirm={confirmAcceptClient}
+        title="Client Accepted"
+        content="Congratulations! You've successfully accepted the client."
+        showButtons={false}
+        icon={
+          <svg width="90" height="90" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M34.364 6.85053C38.6205 -2.28351 51.3795 -2.28351 55.636 6.85053C58.0129 11.951 63.5594 14.6722 68.9556 13.3853C78.6192 11.0807 86.5743 21.2433 82.2185 30.3287C79.7862 35.402 81.1561 41.5165 85.5082 45.0122C93.3019 51.2725 90.4628 63.9451 80.7747 66.1403C75.3648 67.3661 71.5265 72.2695 71.5572 77.9156C71.6123 88.0265 60.1169 93.6664 52.3918 87.3184C48.0781 83.7737 41.9219 83.7737 37.6082 87.3184C29.8831 93.6664 18.3877 88.0266 18.4428 77.9156C18.4735 72.2695 14.6352 67.3661 9.22531 66.1403C-0.462787 63.9451 -3.30193 51.2725 4.49185 45.0122C8.84391 41.5165 10.2138 35.402 7.78151 30.3287C3.42572 21.2433 11.3808 11.0807 21.0444 13.3853C26.4406 14.6722 31.9871 11.951 34.364 6.85053Z" fill="#ECFDF3"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M62.5 32.5C62.5 33.8808 61.3808 35 60 35C58.6192 35 57.5 33.8808 57.5 32.5C57.5 31.1192 58.6192 30 60 30C61.3808 30 62.5 31.1192 62.5 32.5ZM35 32.5C35 33.8808 33.8808 35 32.5 35C31.1192 35 30 33.8808 30 32.5C30 31.1192 31.1192 30 32.5 30C33.8808 30 35 31.1192 35 32.5ZM45.1768 52.5C48.5168 52.5 51.6735 53.75 54.0235 56.1L57.9902 52.1333C54.5635 48.7067 50.0102 47 45.1768 47C40.3435 47 35.7902 48.7067 32.3635 52.1333L36.3302 56.1C38.6802 53.75 41.8368 52.5 45.1768 52.5Z" fill="#039855"/>
+          </svg>
+        }
       />
     </AuthenticatedLayout>
   )
