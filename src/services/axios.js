289c51df57f0ee67bacd3affd19e19d14310811e
shahbazasghar1038 <muhammadshahbaz1038@gmail.com>
2025-05-01 18:39:54 +0500

    change path

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api/", // Replace with your API URL
-  baseURL: "http://13.49.130.107:5000/api/", // Replace with your API URL
+  baseURL: "http://13.49.130.107:5000/api/", // QA server URL
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
