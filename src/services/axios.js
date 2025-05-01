1321737cb98d9dfe7bf06723ea37dd73904fc897
shahbazasghar1038 <muhammadshahbaz1038@gmail.com>
2025-05-01 20:20:57 +0500

    new path

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api/", // Replace with your API URL
-  baseURL: "http://13.49.130.107:5000/api/", // QA server URL
+  // baseURL: "http://13.49.130.107:5000/api/", // QA server
+  baseURL: "https://inj-be.easystepin.io/api/", // QA server
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
