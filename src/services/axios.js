17d6d4df2a008194cc39f8a79e62f40f613c60a0
shahbazasghar1038 <muhammadshahbaz1038@gmail.com>
2025-05-01 20:25:22 +0500

    new path with domiain

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api/", // Replace with your API URL
   // baseURL: "http://13.49.130.107:5000/api/", // QA server
-  baseURL: "https://inj-be.easystepin.io/api/", // QA server
+  baseURL: "https://inj-be.easystepin.io/api/", // QA server with domiain
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
